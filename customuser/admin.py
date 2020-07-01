e4018b3fe8be19b6125ed7cfce49d3a8363894e1
ddnnss <ddnnss.i1@gmail.com>
2020-07-01 21:39:34 +0300

    add tramslations

--- customuser/admin.py
+++ customuser/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "balance",
+        "rating",
+
+    ]
+admin.site.register(User, UserAdmin)
